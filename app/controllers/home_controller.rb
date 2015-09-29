039d6254ff3092160c9c80ead04b6e93c3eabe36
soorajs <soorajs@qburst.com>
2015-09-29 15:09:29 +0530

    updated methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,17 +8,51 @@ class HomeController < ApplicationController
   def scrap
     url = params[:scrap][:url]
     url = 'http://www.google.co.in' if url == ''
-    js = "<script src='//code.jquery.com/jquery-1.11.1.min.js'></script><script>$(document).ready(function()
-           { $('a').click(function(e){e.preventDefault(); alert($(this).attr('href'));
-            window.location.href='/mail?url=$(this).attr('href'); });});</script>".html_safe
-    Rails.logger.info p url
-    open(Rails.root.to_s+ '/scraper.html', 'wb') do |file|
-      open(url) do |uri|
-        file.write(uri.read+js)
-      end
-      FileUtils.mv(Rails.root.to_s+ '/scraper.html', Rails.root.to_s + '/public')
-      redirect_to '/scraper.html'
+    mechanize = Mechanize.new
+    page = mechanize.get(page_url)
+    total_content = "<script type='text/javascript' src= 'https://code.jquery.com/jquery-2.1.4.min.js'></script>".html_safe + page.body.html_safe + script(params[:page])
+    File.open(Rails.root.to_s+'/public/scraper.html', 'wb') { |file| file.write(total_content) }
+    redirect_to '/scraper.html'
+  end
+
+  def image
+    # http://static4.businessinsider.com/image/54228ba76da8110a73d652eb-960/emma-watson.jpg
+    open(CGI::unescape(params[:url]), 'rb') do |f|
+      send_data f.read, :type => f.content_type, :disposition => "inline"
     end
   end
+
+  def script(page)
+    "
+    <script type='text/javascript'>
+    $(function() {
+      var interval = setInterval(change_links,1000);
+      function change_links(){
+        $( 'a' ).each(function( index ) {
+        console.log('polo');
+        var link = $( this ).attr('href'); 
+        if(link){
+          if(link[0] == '/'){
+            $( this ).attr('href', '/get_page?page=http://#{get_host_without_www(page)}'+link);
+          }else{
+            $( this ).attr('href', '/get_page?page='+link);
+          }
+        }
+      });
+      clearInterval(interval);
+      } 
+      
+
+    });
+    </script>
+    ".html_safe
+  end
+
+  def get_host_without_www(url)
+    url = "http://#{url}" if URI.parse(url).scheme.nil?
+    host = URI.parse(url).host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+  end
+
 
 end
